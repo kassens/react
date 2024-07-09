d74094d2514c69af66a63cb4a04bec4334e35468
Jan Kassens <jkassens@meta.com>
2024-07-09 15:27:27 -0400

    Warn about legacy context when legacy context is not disabled
    For environments that still have legacy contexts available, this adds a warning to make the remaining call sites easier to locate and encourage upgrades.
    
    .

--- packages/react-server/src/ReactFizzClassComponent.js
+++ packages/react-server/src/ReactFizzClassComponent.js
@@ -403,6 +403,22 @@ function checkClassInstance(instance: any, ctor: any, newProps: any) {
           name,
         );
       }
+      if (ctor.childContextTypes && !didWarnAboutChildContextTypes.has(ctor)) {
+        didWarnAboutChildContextTypes.add(ctor);
+        console.error(
+          '%s uses the legacy childContextTypes API which will soon be removed. ' +
+            'Use React.createContext() instead.',
+          name,
+        );
+      }
+      if (ctor.contextTypes && !didWarnAboutContextTypes.has(ctor)) {
+        didWarnAboutContextTypes.add(ctor);
+        console.error(
+          '%s uses the legacy contextTypes API which will soon be removed. ' +
+            'Use React.createContext() with static contextType instead.',
+          name,
+        );
+      }
     }
 
     if (typeof instance.componentShouldUpdate === 'function') {
